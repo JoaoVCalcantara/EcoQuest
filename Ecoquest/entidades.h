97e1f5598698c412a350d3e797df9a7e3f3d45f8
NiWYZin <gabrielexplorer0606@gmail.com>
2025-11-12 15:10:54 -0300

    Animação do jogador

--- Ecoquest/entidades.h
+++ Ecoquest/entidades.h
@@ -1,17 +1,26 @@
 #ifndef ENTIDADES_H
 #define ENTIDADES_H
 
-#include <stdbool.h>
-#include <allegro5/keyboard.h>
 #include <allegro5/allegro_color.h>
+#include <allegro5/keyboard.h>
+#include <stdbool.h>
+#include "sprites.h"
 
 /* forward declarations */
 struct CavernaState;
 struct AllegroContext; /* necessário porque alguns protótipos usam AllegroContext* */
 
 /* Constants */
-#define PLAYER_RADIUS 35.0f
+#define PLAYER_RADIUS 15.0f
 #define PLAYER_SPEED 2.0f
+
+typedef enum {
+    DIRECAO_CIMA,
+	DIRECAO_BAIXO,
+    DIRECAO_ESQ,
+	DIRECAO_DIR
+} Direcao;
+
 
 /* Estrutura completa das entidades */
 typedef struct {
@@ -19,6 +28,20 @@ typedef struct {
     float y;
     float raio;
     float velocidade;
+
+	SpriteAnimadoFrameAFrame* sprite_cima;
+    SpriteAnimadoFrameAFrame* sprite_baixo;
+    SpriteAnimadoFrameAFrame* sprite_direita;
+    SpriteAnimadoFrameAFrame* sprite_esquerda;
+
+    SpriteAnimadoFrameAFrame* sprite_idle_cima;
+    SpriteAnimadoFrameAFrame* sprite_idle_baixo;
+    SpriteAnimadoFrameAFrame* sprite_idle_esquerda;
+    SpriteAnimadoFrameAFrame* sprite_idle_direita;
+
+    Direcao Direcao_atual;
+    bool movendo;
+    bool usar_sprite;
 } entidade;
 
 /* Dar tag à struct camera para permitir forward declarations */
@@ -34,5 +57,6 @@ void desenhar_jogador(const entidade* p, ALLEGRO_COLOR cor);
 void limitar_jogador(entidade* jogador, float largura_mapa, float altura_mapa);
 void processar_teclado(ALLEGRO_KEYBOARD_STATE* estado, entidade* jogador);
 bool colisao(float cx, float cy, float r, float rx, float ry, float rw, float rh);
+void destruir_entidade(entidade* p);
 
 #endif // ENTIDADES_H
--- Ecoquest/main.c
+++ Ecoquest/main.c
@@ -9,10 +9,12 @@
 #include "menu.h"
 #include "batalha.h"
 #include "inimigo.h"
+#include "sprites.h"
 
 #define MAX_BOTS 4
 
 int main(void) {
+
     // Inicializa gerador de numeros aleatorios para IA dos bots
     srand((unsigned int)time(NULL));
 
@@ -28,9 +30,50 @@ int main(void) {
         return 0;
     }
 
+    const char* frames_jogador_cima[] = {
+    "assets/img/Heroi/rear_0.png",
+    "assets/img/Heroi/rear_1.png"
+    };
+    const char* frames_jogador_baixo[] = {
+    "assets/img/Heroi/front_0.png",
+    "assets/img/Heroi/front_1.png"
+    };
+    const char* frames_jogador_direita[] = {
+    "assets/img/heroi/player_right_0.png",
+    "assets/img/heroi/player_right_1.png"
+    };
+    const char* frames_jogador_esquerda[] = {
+    "assets/img/heroi/player_left_0.png",
+    "assets/img/heroi/player_left_1.png"
+    };
+    const char* arr_idle_baixo[] = { "assets/img/Heroi/idle_down.png" };
+    const char* arr_idle_cima[] = { "assets/img/Heroi/idle_up.png" };
+    const char* arr_idle_direita[] = { "assets/img/Heroi/idle_right.png" };
+    const char* arr_idle_esquerda[] = { "assets/img/Heroi/idle_left.png" };
+
     // -------- INICIALIZA O JOGO --------
     entidade jogador;
     iniciar_entidade(&jogador, (float)ctx.width, (float)ctx.height);
+
+	jogador.sprite_cima = criar_sprite_animado_array(frames_jogador_cima, 2, 0.2f);
+	jogador.sprite_baixo = criar_sprite_animado_array(frames_jogador_baixo, 2, 0.2f);
+	jogador.sprite_esquerda = criar_sprite_animado_array(frames_jogador_esquerda, 2, 0.2f);
+	jogador.sprite_direita = criar_sprite_animado_array(frames_jogador_direita, 2, 0.2f);
+
+    jogador.sprite_idle_baixo = criar_sprite_animado_array(arr_idle_baixo, 1, 1.0f);
+    jogador.sprite_idle_direita = criar_sprite_animado_array(arr_idle_direita, 1, 1.0f);
+    jogador.sprite_idle_esquerda = criar_sprite_animado_array(arr_idle_esquerda, 1, 1.0f);
+
+    const char* arr_idle_cima_fallback[] = { "assets/img/Heroi/rear_0.png" };
+    jogador.sprite_idle_cima = criar_sprite_animado_array(arr_idle_cima_fallback, 1, 1.0f);
+
+    // Ativa sprites
+    jogador.usar_sprite = sprite_animado_frames_valido(jogador.sprite_baixo) &&
+        sprite_animado_frames_valido(jogador.sprite_cima);
+
+    if (!jogador.usar_sprite) {
+		printf("Aviso: Sprites do jogador nao carregaram corretamente. Usando circulo simples.\n");
+    }
     
     // Cria bots em diferentes cenarios
     Bot bots[MAX_BOTS];
@@ -63,6 +106,41 @@ int main(void) {
             al_get_keyboard_state(&estado_teclado);
             processar_teclado(&estado_teclado, &jogador);
             limitar_jogador(&jogador, 1280.0f, 720.0f);
+
+            if (jogador.usar_sprite) {
+                if (jogador.movendo) {
+                    switch (jogador.Direcao_atual) {
+                    case DIRECAO_CIMA:
+                        atualizar_sprite_animado_frames(jogador.sprite_cima, delta_time);
+                        break;
+                    case DIRECAO_BAIXO:
+                        atualizar_sprite_animado_frames(jogador.sprite_baixo, delta_time);
+                        break;
+                    case DIRECAO_ESQ:
+                        atualizar_sprite_animado_frames(jogador.sprite_esquerda, delta_time);
+                        break;
+                    case DIRECAO_DIR:
+                        atualizar_sprite_animado_frames(jogador.sprite_direita, delta_time);
+                        break;
+                    }
+                }
+                else {
+                    switch (jogador.Direcao_atual) {
+                    case DIRECAO_CIMA:          // ← ADICIONAR
+                        atualizar_sprite_animado_frames(jogador.sprite_idle_cima, delta_time);
+                        break;
+                    case DIRECAO_BAIXO:
+                        atualizar_sprite_animado_frames(jogador.sprite_idle_baixo, delta_time);
+                        break;
+                    case DIRECAO_ESQ:
+                        atualizar_sprite_animado_frames(jogador.sprite_idle_esquerda, delta_time);
+                        break;
+                    case DIRECAO_DIR:
+                        atualizar_sprite_animado_frames(jogador.sprite_idle_direita, delta_time);
+                        break;
+                    }
+                }
+            }
             
             // Atualiza todos os bots
             for (int i = 0; i < MAX_BOTS; i++) {
@@ -120,7 +198,7 @@ int main(void) {
             al_flip_display();
         }
     }
-
+	destruir_entidade(&jogador);
     al_destroy_font(fonte);
     destruir_allegro(&ctx);
     return 0;
